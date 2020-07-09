24a8fd5b808bbb9d4ddd1f22308fbe89256037bf
Gustav Holmsten <gustav.holmsten@blinfo.se>
2020-07-09 07:06:02 +0200

    first commit

---
+++ .doom.d/config.el
@@ -0,0 +1,140 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+;; Load personal modules
+  (load! "+javascript")
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Gustav Holmsten"
+      user-mail-address "gustav.holmsten@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; test
+(setq doom-font (font-spec :family "Hack" :size 16)
+      doom-variable-pitch-font (font-spec :family "Hack"))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-one)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/Documents/")
+(require 'org)
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type 'relative)
+
+;; To get typescript syntax higilightin
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(add-to-list 'load-path (expand-file-name "~/.doom.d/lisp"))
+(add-to-list 'custom-theme-load-path "~/.doom.d/themes")
+
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'tide-mode-hook 'prettier-js-mode)
+(add-hook 'markdown-mode-hook 'grip-mode)
+(add-hook 'org-mode-hook 'grip-mode)
+
+(use-package! org-super-agenda
+  :after org-agenda
+  :init
+  (setq org-super-agenda-groups '((:name "Today"
+                                         :time-grid t
+                                         :scheduled today)
+                                  (:name "Due today"
+                                         :deadline today)
+                                  (:name "Important"
+                                         :priority "A")
+                                  (:name "Overdue"
+                                         :deadline past)
+                                  (:name "Due soon"
+                                         :deadline future)
+                                  (:name "Big Outcomes"
+                                         :tag "bo")))
+  :config
+  (org-super-agenda-mode))
+
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
+
+;; Recenter on next/prev line
+;; (defun my-evil-next-line-and-recenter (&optional count)
+;;   (interactive)
+;;   (evil-next-line count)
+;;   (recenter))
+;; (defun my-evil-previous-line-and-recenter (&optional count)
+;;   (interactive)
+;;   (evil-previous-line count)
+;;   (recenter))
+
+;; ;; Remap next/prev line to the above
+;; (map! [remap evil-next-line] #'my-evil-next-line-and-recenter
+;;       [remap evil-previous-line] #'my-evil-previous-line-and-recenter)
+
+;; Keymaps
+(map! :leader
+      (:prefix "t"
+        :nv "f" #'tide-fix
+        :nv "r" #'tide-references
+        :nv "d" #'tide-jump-to-definition
+        :nv "e" #'tide-goto-error
+        :nv "b" #'tide-jump-back
+        :nv "s r" #'tide-rename-symbol
+        :nv "c f" #'tide-code-fix)
+      (:prefix "ö"
+        :nv "f w" #'deadgrep
+        :nv "f f" #'counsel-fzf
+        :nv "p s" #'+ivy/project-search)
+      (:prefix "v"
+        :nv "f" #'vimish-fold-toggle
+        :nv "c f" #'vimish-fold-avy
+        :nv "n f" #'vimish-fold-next-fold
+        :nv "t a" #'vimish-fold-toggle-all
+        :nv "u a" #'vimish-fold-unfold-all
+        :nv "r a" #'vimish-fold-refold-all)
+      (:prefix "a"
+        :nv "f" #'avy-goto-char-2
+        :nv "m l" #'avy-move-line
+        :nv "m r" #'avy-move-region
+        :nv "c r" #'avy-copy-region
+        :nv "c l" #'avy-copy-line))
+
+(map! :nmv "J" (lambda! (evil-next-line 5))
+      :nmv "K" (lambda! (evil-previous-line 5)))
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
---
+++ .zshrc
@@ -0,0 +1,74 @@
+
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+
+ZSH_THEME="sunrise"
+SOLARIZED_THEME="light"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+ COMPLETION_WAITING_DOTS="true"
+
+    # Set fzf installation directory path
+# export FZF_BASE=/path/to/fzf/install/dir
+export FZF_CTRL_T_COMMAND='rg --files --hidden --follow'
+export FZF_DEFAULT_OPTS='--height 50% --reverse --extended --preview "head -100 {}"'
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git
+zsh-z
+git
+fzf
+zsh-autosuggestions)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+ if [[ -n $SSH_CONNECTION ]]; then
+   export EDITOR='vim'
+ else
+   export EDITOR='nvim'
+ fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Aliases
+alias reddit="tuir --enable-media"
+alias zconfig="emacs ~/.zshrc&"
+alias etcher="~/Documents/appimages/balenaEtcher-1.5.81-x64.AppImage&"
+alias pagraph="~/Downloads/pagraphcontrol/dist/pagraphcontrol-linux-x64/pagraphcontrol&"
+alias omzconfig="emacs ~/.oh-my-zsh&"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias reload='source ~/.zshrc'
+alias doomrefresh='~/.emacs.d/bin/doom refresh'
+alias lsa='ls -a -thor'
+alias ls='ls -thor'
+alias mm='xrandr --output HDMI-0 --off --output DP-2.1 --primary --mode 2560x1440 --pos 1920x152 --rotate normal --output DP-2.2 --mode 2560x1440 --pos 4480x0 --rotate left --output DP-2 --off --output DP-1 --mode 1920x1080 --pos 0x512 --rotate normal --output DP-0 --off'
+
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+. $HOME/.asdf/asdf.sh
+
+. $HOME/.asdf/completions/asdf.bash
+
+source  ~/powerlevel9k/powerlevel9k.zsh-theme
