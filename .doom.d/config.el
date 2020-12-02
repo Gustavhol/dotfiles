0dd4c53aacb7b840d13d1a88452947f4bc271de8
Gustavhol <gustav.holmsten@blinfo.se>
2020-12-02 06:14:19 +0100

    something or the other

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,7 +21,7 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; test
-(setq doom-font (font-spec :family "Hack" :size 16)
+(setq doom-font (font-spec :family "Hack" :size 14)
       doom-variable-pitch-font (font-spec :family "Hack"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
