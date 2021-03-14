e9851767e800edcce74ba2fa9504a48dc7373fd4
Gustavhol <gustav.holmsten@blinfo.se>
2021-03-14 09:26:44 +0100

    refined polybar and added fonts

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,9 +5,17 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
- polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+  done
+else
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+fi
+ 
 
 echo "Bars launched..."
