a961e9e9e4c9fd37d1b92b17c784d7902b04ad41
Gustavhol <gustav.holmsten@blinfo.se>
2021-05-17 12:21:39 +0200

    removed hostory file again

--- .config/zsh/add_history.zsh
+++ .config/zsh/add_history.zsh
@@ -8,5 +8,5 @@
 
  # Adds every command per working directory in ./zsh_history_ext for later referencing
 function zshaddhistory() {
-	echo "${1%%$'\n'}|${PWD}   " >> ~/scripts/logs/.zsh_history_ext
+	echo "${1%%$'\n'}|${PWD}   " >> ~/.zsh_history_ext
 }
