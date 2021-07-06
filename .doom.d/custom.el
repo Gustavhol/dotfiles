f59defcd54c187adbb57167355f0220a5fc45df6
Gustavhol <gustav.holmsten@blinfo.se>
2021-07-06 08:38:54 +0200

    update doom config

---
+++ .doom.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(lsp-tailwindcss cloc grip-mode lsp-ui))
+ '(wakatime-api-key "4e468cc9-62d5-4197-9cf6-69b4f0109ee4")
+ '(warning-suppress-log-types '((lsp-mode) (lsp-mode) (lsp-mode) (lsp-mode)))
+ '(warning-suppress-types '((lsp-mode) (lsp-mode) (lsp-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
